97984d139cc318327e4f5de0755ffdfb1a6fae0b
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2024-10-01 16:15:45 -0400

    made a countOccurrences method using for each loop.

--- java101/Main.java
+++ java101/Main.java
@@ -1,16 +1,7 @@
+package java101;
 public class Main {
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 3, 4, 5};
-		System.out.println(intArrToString(arr));
-	}
-	static String intArrToString(int[]arr) {
-		String str = "(";
-		for (int i = 0; i < arr.length; i++) {
-			str += arr[i];
-			if (i != arr.length - 1) {
-				str += ", ";
-			}
-		}
-		return str + ")";
+	int[] arr = {1, 1, 4, 5, 6, 6, 9};
+	System.out.println(PracticeProblems.countOccurrences(arr, 7));
 	}
 }
---
+++ java101/PracticeProblems.java
@@ -0,0 +1,42 @@
+package java101;
+public class PracticeProblems {
+    public static void main(String[] args){
+
+    }
+    static double f(double x) {
+        return 2 * x;
+    }
+
+    static void printIntArray(int[] arr) {
+        String str = "(";
+        for (int i = 0; i < arr.length; i++) {
+            str += arr[i];
+            if (i != arr.length - 1) {
+                str += ", ";
+            }
+        }
+        str += ")";
+        System.out.println(str);
+    }
+
+    static String intArrToString(int[] arr) {
+        String str = "(";
+        for (int i = 0; i < arr.length; i++) {
+            str += arr[i];
+            if (i != arr.length - 1) {
+                str += ", ";
+            }
+        }
+        return str + ")";
+    }
+    static int countOccurrences(int[] arr, int n){
+        int counter = 0;
+        for (int x : arr){
+            if (x == n){
+                counter ++;
+            }
+        }
+        return counter;
+    }
+}
+
