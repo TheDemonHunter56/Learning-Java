27dea9b87cde9123e20f985c73dd683bae53af59
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2024-10-04 16:11:19 -0400

    started the Point class

--- java101/Main.java
+++ java101/Main.java
@@ -1,14 +1,6 @@
 package java101;
 public class Main {
 	public static void main(String[] args) {
-	int[][] arr = {
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9}
-	};
-	System.out.println(PracticeProblems.fib(7));
 	
 	}
 }
-
-//0 1 1 2 3 5 8 13
--- java102/Main.java
+++ java102/Main.java
@@ -1,4 +1,7 @@
 package java102;
 public class Main {
-    
+    public static void main(String[] args) {
+        Point point = new Point(4, 3);
+        System.out.println("x: " + point.x + ", y: " + point.y);
+    }
 }
