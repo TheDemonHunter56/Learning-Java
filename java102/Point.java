488b0b8e869d58b0bca6217c4dc88a726889008c
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2024-10-07 16:43:19 -0400

    saving

--- java102/Point.java
+++ java102/Point.java
@@ -20,7 +20,25 @@ public class Point {
 	public String toString() {
 		return "(" + x + ", " + y + ")";
 	}
+
 	public static double distance(Point p1, Point p2) {
 		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+	}
+
+	public static Point centerOfMass(Point[] points){
+		double xCoord = 0;
+		double xSum = 0;
+		double yCoord = 0;
+		double ySum = 0;
+		double counter = 0;
+		for (int i = 0; i < points.length; i ++){
+			xSum += points[0 + i].x;
+			ySum += points[0 + i].y;
+			counter += 1;
+		}
+		yCoord = ySum / counter;
+		xCoord = xSum/ counter;
+		Point centOfMass = new Point(xCoord, yCoord);
+		return centOfMass;
 	}
 }
