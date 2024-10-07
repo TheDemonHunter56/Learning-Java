073453aacc6e702ced3679172244e55f102bc7c9
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2024-10-07 16:42:44 -0400

    added more to the point class (distance/center of mass)

--- java102/Main.java
+++ java102/Main.java
@@ -2,6 +2,8 @@ package java102;
 public class Main {
     public static void main(String[] args) {
         Point point = new Point(4, 3);
-        System.out.println("x: " + point.x + ", y: " + point.y);
+        Point a = new Point(1, 5);
+        Point b = new Point(4, 2);
+        System.out.println(Point.distance(a, b));
     }
 }
--- java102/Point.java
+++ java102/Point.java
@@ -15,4 +15,12 @@ public class Point {
 	public Point translateY(double t) {
 		return new Point(x, y + t);
 	}
+	
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+	}
 }
