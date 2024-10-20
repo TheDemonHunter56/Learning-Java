a32c26368f7d48a6dc0b3b66358a013e94797abb
Dimitri Lesokhin <dimitrilesokhin09@gmail.com>
2024-10-19 22:25:45 -0400

    stuff. Why am I adding a message again?

--- java102/Main.java
+++ java102/Main.java
@@ -1,24 +1,12 @@
 package java102;
 import java.util.ArrayList;
 
+import java102.GridClass.Grid;
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList<>();
-        arr.add("Hello");
-        arr.add("World");
-        arr.set(0, "Goodbye");
-        
-        ArrayList<Boolean> conditions = new ArrayList<>();
-        conditions.add(true);
-        conditions.add(arr.get(0) == "Hello");
-        conditions.add(conditions.get(0) || conditions.get(1));
-
-        ArrayList<Integer> intArr = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            intArr.add(0);
-        }
-        System.out.println(intArr.size());
-        System.out.println(intArr.get(32));
+        Grid<Integer> grid = new Grid<>(5, 0);
+        grid.set(2, 2, 4);
+        System.out.println(grid);
     }
-    
 }
